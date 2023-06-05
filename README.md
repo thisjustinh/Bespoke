86189ca9df2f6472a0f544109e3a529371f43245
Huang, Justin <jfh5730@psu.edu>
2023-06-04 23:03:29 -0400

    README changes before commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bespoke
-LLM news article summarizer?
+LLM news article summarizer! The plan is to fine-tune the [tiiuae/falcon-7b](https://huggingface.co/tiiuae/falcon-7b) on the Cornell Newsroom dataset and hope that it performs well.
+
+## Supported Sources
+Based on very legit examination of `robots.txt`, our current supported news sites are the following:
+
+- [Forbes](https://www.forbes.com/)
+- [NPR](https://www.npr.org/)
--- src/chain.py
+++ src/chain.py
@@ -5,11 +5,12 @@ from scraper.forbes import get_forbes_article
 # Set API Key
 load_dotenv(find_dotenv())
 
-falcon_instruct = HuggingFaceHub(repo_id='google/flan-t5-xl')
+falcon_instruct = HuggingFaceHub(repo_id='tiiuae/falcon-7b')
 
 forbes_article = get_forbes_article('https://www.forbes.com/sites/kenrickcai/2023/06/04/stable-diffusion-emad-mostaque-stability-ai-exaggeration/?sh=5fad3cc175c5')
 
-# TODO: This currently doesn't work LOL.
-prompt = f"The article is as follows: {forbes_article}. The summary is "
+# TODO: This currently doesn't work LOL
+prompt = f"The article is as follows:\n\n{forbes_article}. The summary is "
+print(prompt)
 completion = falcon_instruct(prompt)
 print(completion)
